9fad8f0cf3847a018e91c2f3a5d348a809b3a978
ozgebilici <biliciozge17@gmail.com>
2023-10-24 10:02:31 +0300

    Add project files.

---
+++ Menu_veri/Liste.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_veri
+{
+    internal class Liste
+    {
+        Node head = null;
+        
+        public void BasaEkle()
+        {
+            Console.Write("Listeye Eklenecek Sayıyı Giriniz:");
+           int  sayi=Convert.ToInt32(Console.ReadLine());
+
+            Node eleman = new Node();
+            eleman.data = sayi;
+
+            if (head == null)
+            {
+                head = eleman;
+                Console.WriteLine(head.data+"  ilk eleman olarak eklendi.");
+            }
+
+            else
+            {
+               eleman.next = head;
+                head = eleman;
+                
+                Console.WriteLine(head.data+"  başa eklendi");
+            }
+        }
+
+        public void SonaEkle()
+        {
+            
+            Console.WriteLine("Sona eklemek istediğiniz sayıyı giriniz");
+            int sayı= Convert.ToInt32(Console.ReadLine());
+
+            Node eleman = new Node();
+            eleman.data = sayı;
+            if(head == null)
+            {
+                head = eleman;
+                Console.WriteLine("liste boş olduğu için "+eleman.data+" ilk eleman olarak eklendi ");
+            }
+
+            else
+            {
+                Node temp= head;
+
+                while(temp.next != null) { 
+                    temp = temp.next;
+                }
+                temp.next = eleman;
+
+                Console.WriteLine(eleman.data+"  sayısı sona eklendi");
+            }
+        }
+
+        public void bastanSil()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Eleman olmadığı için silemiyoruz.");
+            }
+            else
+            {
+                Console.WriteLine(head.data+" silindi");
+                head = head.next;
+                
+            }
+        }
+
+        public void sondanSil()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Eleman olmadığı için silemiyoruz.");
+            }
+            else if (head.next == null)
+            {
+                // Listenin sadece bir elemanı varsa, bu elemanı silin.
+                Console.WriteLine(head.data + " silindi");
+                head = null;
+            }
+            else
+            {
+                Node temp = head;
+                Node temp2 = null;
+
+                while (temp.next != null)
+                {
+                    temp2 = temp;
+                    temp = temp.next;
+                   
+                }
+                temp2.next = null;
+
+                Console.WriteLine(temp.data + " silindi");
+            }
+        }
+
+        public void yazdir()
+        {
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine("liste boş");
+            }
+            else
+            {
+                Console.Write("baş-->");
+               
+                while(temp.next != null)
+                {
+                    Console.Write(temp.data + "-->");
+                    temp= temp.next;
+                    
+                }
+                Console.Write(temp.data);
+                Console.WriteLine("-->son");
+            }
+        }
+
+        public void arayaEkle() 
+        {
+            Console.WriteLine("Sayıyı kaçıncı sıraya eklemek istersiniz?");
+            int yer= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("hangi sayıyı eklemek istersiniz?");
+            int sayi= Convert.ToInt32(Console.ReadLine());
+            Node temp = head;
+
+            if (yer == 0)
+            {
+                Console.WriteLine("hatalı yer seçimi");
+            }
+            else 
+            {
+                for (int i = 1; i < yer - 1; i++)
+                {
+                    temp = temp.next;
+                }
+                Node eleman = new Node();
+                eleman.data = sayi;
+                eleman.next = temp.next;//eklediğim sayıdan sonra sayılar devam etsin diye 
+                temp.next = eleman;//araya eklediğim kısım
+
+                Console.WriteLine(yer + ".sıraya " + eleman.data + " yerleştirildi.");
+            }
+            
+
+        }
+
+        public void aradanSil()
+        {
+            Console.WriteLine(" kaçıncı sırayı silmek istersiniz?");
+            int yer = Convert.ToInt32(Console.ReadLine());
+            Node temp = head;
+            Node temp2 = temp;
+
+            if (yer == 0)
+            {
+                Console.WriteLine("hatalı yer seçimi");
+            }
+            else if (yer==1)
+            {
+                Console.WriteLine(head.data + " ilk eleman silindi");
+                head = head.next;
+               
+            }
+            else
+            {
+                for (int i = 0; i < yer-1; i++)
+                {
+                    temp2 = temp;
+                    temp = temp.next;
+                }
+                if(temp2.next==null)
+                {
+                    Console.WriteLine(" o satırda eleman yok");
+                }
+                else
+                {
+                    temp2.next = temp.next;
+                }
+           
+                Console.WriteLine(yer + ".sıradan " + temp.data + " çıkarıldı.");
+            }
+        }
+
+        public void ListeSay()
+        {
+            Node temp = head;
+            int sayac=1;
+            while(temp.next != null)
+            {
+                temp=temp.next;
+                sayac++;
+            }
+            Console.WriteLine("listede "+sayac+" sayı var.");
+        }
+
+    }
+}
---
+++ Menu_veri/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace Menu_veri
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Liste ilkliste = new Liste();
+            int secim;
+
+            do
+            {
+                secim = Menu();
+
+                switch (secim)
+                {
+                    case 1:
+                        ilkliste.BasaEkle();
+                        break;
+
+                    case 2:
+                        ilkliste.SonaEkle();
+                        break;
+                    
+                    case 3:
+                        ilkliste.sondanSil();
+                        break;
+                    case 4:
+                        ilkliste.bastanSil();
+                        break;
+                    case 5:
+                        ilkliste.arayaEkle();
+                        break;
+                    case 6:
+                        ilkliste.aradanSil();
+                        break;
+                    case 7:
+                        ilkliste.ListeSay();
+                        break;
+                    case 8: 
+                        ilkliste.yazdir();
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz seçim, tekrar deneyin.");
+                        break;
+                }
+            } while (secim != 0);
+        }
+
+        private static int Menu()
+        {
+            Console.WriteLine("İstediğiniz işlemi seçiniz:");
+            Console.WriteLine("1--> Başa sayı ekleme");
+            Console.WriteLine("2--> Sona sayı ekleme");
+            Console.WriteLine("3--> Sondan silme");
+            Console.WriteLine("4--> Baştan silme");
+            Console.WriteLine("5--> Araya sayı ekleme");
+            Console.WriteLine("6--> Aradan silme");
+            Console.WriteLine("7--> Sayıları sayma");
+            Console.WriteLine("8--> Yazdır");
+            Console.WriteLine("0--> Çıkış");
+            int secim = int.Parse(Console.ReadLine());
+            return secim;
+        }
+    }
+}
